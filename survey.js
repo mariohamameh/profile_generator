1565c8d1db6e73c8ea57d9195422906a22dbb513
mariohamameh <mariohamameh97@gmail.com>
2022-02-16 05:17:54 +0000

    added survey.js

---
+++ survey.js
@@ -0,0 +1,51 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  rl.question('What is your name? Nicknames are also acceptable :)', (answer) => {
+    console.log(`Thank you for your valuable feedback, ${answer}`);
+    rl.question('What is your superpower?', (answer) => {
+        console.log(`Thank you for your valuable feedback: ${answer}`);
+        rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer) => {
+            console.log(`Thank you for your valuable feedback: ${answer}`);
+        rl.close();
+            });
+        });
+    });
+});
+
+/*
+rl.question('What is your name? Nicknames are also acceptable :)', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+    rl.close();
+});
+
+rl.question('What is an activity you like doing?', (answer) => {
+    console.log(`What do you listen to while doing that? ${answer}`);
+  
+    rl.close();
+});
+
+rl.question('What is your superpower?', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+    rl.close();
+});
+rl.question('Which sport is your absolute favourite?', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+    rl.close();
+});
+  
+rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+    rl.close();
+});  
+*/
